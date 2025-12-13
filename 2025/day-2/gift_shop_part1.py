78c0cde5843ba420f796291294a3527f84485ef9
lucas-alencar <lucasalencar.rd@gmail.com>
2025-12-13 19:35:25 -0300

    Day 1 - Part 2

---
+++ 2025/day-2/gift_shop_part1.py
@@ -0,0 +1,68 @@
+class InvalidIDRange(Exception):
+    pass
+
+class IDChecker:
+    def check_id_file(self, file_name: str):
+        ranges: list[tuple[int, int]] = self._get_ranges_from_file(file_name)
+        self.invalid_IDs: list[int] = self._get_invalid_IDs(ranges)
+
+    def _get_invalid_IDs(self, ranges) -> list[int]:
+        invalid_IDs = []
+        for begin, end in ranges:
+            for i in range(begin, end + 1):
+                if self._is_invalid_id(str(i)):
+                    print("Added: ", i)
+                    invalid_IDs.append(i)
+
+        return invalid_IDs
+
+    def  _is_invalid_id(self, id: str) -> bool:
+        id_len = len(id)
+
+        if id_len < 2:
+            return False
+
+        if id_len % 2 != 0:
+            return False
+
+        if id.count(id[0:(id_len // 2)]) == 2:
+            return True
+
+        return False
+
+    def _get_ranges_from_file(self, file_name: str) -> list[tuple[int, int]]:
+        ranges: list[tuple[int,int]] = []
+
+        try:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                file_content = file.read().strip()
+                raw_pairs = file_content.split(',')
+
+                for pair in raw_pairs:
+                    parts = pair.split('-')
+                    try:
+                        if ( parts[0][0] == "0" ) or ( parts[1][0] == "0" ):
+                            continue
+
+                        start = int(parts[0])
+                        end = int(parts[1])
+                        ranges.append((start, end))
+                    except(ValueError, IndexError):
+                        raise InvalidIDRange("An error has occoured on attempt to read a pair : pair=", {pair})
+
+        except FileNotFoundError:
+            print(f"Error: '{file_name}' is not a valid file name")
+
+        return ranges
+
+if __name__ == "__main__":
+    invalid_product_ID = []
+    id_checker = IDChecker()
+    id_checker.check_id_file("input.txt")
+
+    sum = 0
+    for i in id_checker.invalid_IDs:
+        sum += i
+
+    print("Computer ended. Result=", sum)
+
